4b0fc9144c2f6b9892fab72ffee4ccc3f9fa9bb9
João Gabriel <joao.mira@fatec.sp.gov.br>
2023-07-05 22:04:59 -0300

    created NavigationTopBar

--- quero_ingresso/src/views/Home.js
+++ quero_ingresso/src/views/Home.js
@@ -1,7 +1,12 @@
 import NavigationTopBar from "../components/NavigationTopBar";
+import NavigationSideBar from "../components/NavigationSideBar/NavigationSideBar"
+import "./home.css"
 
 export default function Home() {
     return(
+        <>
         <NavigationTopBar />
+        <NavigationSideBar />
+        </>
     )
 }
