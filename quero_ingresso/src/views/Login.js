bd5601c578413fbf307fef786def3f151d112101
Rebeca Gama <rebeca.moraes@fatec.sp.gov.br>
2023-07-06 13:40:10 -0300

    form login

--- quero_ingresso/src/views/Login.js
+++ quero_ingresso/src/views/Login.js
@@ -5,45 +5,53 @@ import "./login.css";
 export default class Login extends Component {
   render() {
     return (
-      <form>
-        <li className='logo' style={{float: 'center'}}>
-            <img src={logo} alt='home' />
-        </li>
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Login"
-          />
-        </div>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <form>
+              <li className='logo' style={{float: 'center'}}>
+                  <img src={logo} alt='home' />
+              </li>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Login"
+                />
+              </div>
 
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Senha"
-          />
-        </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Senha"
+                />
+              </div>
 
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Lembrar-me
-            </label>
+              <div className="mb-3">
+                <div className="custom-control custom-checkbox">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="customCheck1"
+                  />
+                  <label className="custom-control-label" htmlFor="customCheck1">
+                    Lembrar-me
+                  </label>
+                </div>
+              </div>
+
+              <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">
+                  <a href="/home"> Entrar</a>
+                  </button>
+              </div>
+            </form>
+            
           </div>
         </div>
+      </div>
 
-        <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-            <a href="/home"> Entrar</a>
-            </button>
-        </div>
-      </form>
     )
   }
 }
