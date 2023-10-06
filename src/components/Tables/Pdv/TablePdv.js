6ed61fbb23ea441a0de6c10b1ce31213d91eb48d
Rebeca Gama <rebeca.moraes@fatec.sp.gov.br>
2023-10-06 10:45:36 -0300

    Update TablePdv.js

--- src/components/Tables/Pdv/TablePdv.js
+++ src/components/Tables/Pdv/TablePdv.js
@@ -1,10 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './tablePDV.css';
 import { TableContainer } from '@mui/material';
 import TableCell from '@mui/material/TableCell';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import { visuallyHidden } from '@mui/utils';
 import TablePagination from '@mui/material/TablePagination';
+import Connection from '../../../model';
+
+const TablePDV = () => {
+  const [pdvs, setPdvs] = useState([]); // Estado para armazenar dados da rota
+  const [dataLoaded, setDataLoaded] = useState(false); // Estado para controlar se os dados foram carregados
+
+  // Recupera o objeto do evento selecionado do localStorage
+  const selectedEventCodeJSON = localStorage.getItem("selectedEvent");
+  const selectedEventCode = JSON.parse(selectedEventCodeJSON); // Converte a string JSON em um objeto
+
+  //console.log(selectedEventCode);
+  //console.log(selectedEventCode.eve_cod);
+
+  useEffect(() => {
+    if (selectedEventCode && !dataLoaded) {
+      const conn = Connection();
+  
+      // Acessa o endpoint de tipo de ingresso
+      const fetchPdvs = async () => {
+        try {
+          const response = await conn.get(
+            'eventos/pdvs?evento=' +
+              selectedEventCode.eve_cod,
+            {
+              headers: {
+                'token': localStorage.getItem('token')
+              }
+            }
+          );
+  
+          // Se der certo, salva os dados no estado de tipo de ingresso
+          if (response.status === 200) {
+            setPdvs(response.data);
+            setDataLoaded(true)
+          } else {
+            console.log('Erro na resposta da API (Tipo Ingresso):', response);
+          }
+        } catch (error) {
+          console.error('Erro na solicitação GET (Tipo Ingresso):', error);
+        }
+      };
+
+      fetchPdvs();
+    }
+  }, [selectedEventCode, dataLoaded]);
 
 // Funções de ordenação
 function descendingComparator(a, b, orderBy) {
@@ -114,15 +159,15 @@ const SortableTableCell = (props) => {
   );
 };
 
-const TablePDV = () => {
-  const [tabelaData, setTabelaData] = useState([
+const [tabelaData, setTabelaData] = useState([])
+/*const TablePDV = () => {
     { id: 1, pdv: 'Loja A', qtdeHoje: 10, valorHoje: 'R$ 500,00', qtdeTotal: 100, valorTotal: 'R$ 5000,00', cortesia: 0, pgto: 'R$ 0,00' },
     { id: 2, pdv: 'Loja B', qtdeHoje: 5, valorHoje: 'R$ 250,00', qtdeTotal: 50, valorTotal: 'R$ 2500,00', cortesia: 0, pgto: 'R$ 0,00' },
     { id: 3, pdv: 'Loja C', qtdeHoje: 8, valorHoje: 'R$ 400,00', qtdeTotal: 80, valorTotal: 'R$ 4000,00', cortesia: 0, pgto: 'R$ 0,00' },
     { id: 4, pdv: 'Loja D', qtdeHoje: 3, valorHoje: 'R$ 150,00', qtdeTotal: 30, valorTotal: 'R$ 1500,00', cortesia: 0, pgto: 'R$ 0,00' },
     { id: 5, pdv: 'Loja E', qtdeHoje: 6, valorHoje: 'R$ 300,00', qtdeTotal: 60, valorTotal: 'R$ 3000,00', cortesia: 0, pgto: 'R$ 0,00' },
     { id: 6, pdv: 'Loja F', qtdeHoje: 4, valorHoje: 'R$ 200,00', qtdeTotal: 40, valorTotal: 'R$ 2000,00', cortesia: 0, pgto: 'R$ 0,00' }
-  ]);
+  ]);*/
 
   const [linhaSelecionada, setLinhaSelecionada] = useState(-1);
   const [order, setOrder] = useState('asc');
@@ -130,17 +175,17 @@ const TablePDV = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const expandirLinha = (id) => {
-    setTabelaData((prevData) =>
+  const expandirLinha = (pdv) => {
+    setPdvs((prevData) =>
       prevData.map((item) => {
-        if (item.id === id) {
+        if (item.pdv === pdv) {
           return { ...item, expandir: !item.expandir };
         }
         return item;
       })
     );
 
-    setLinhaSelecionada(id);
+    setLinhaSelecionada(pdvs);
   };
 
   const handleRequestSort = (event, property) => {
@@ -162,10 +207,11 @@ const TablePDV = () => {
     return { pdv, vendas, cortesias, impressos, cancelados, pgto, valor };
   }
   
-  const rows = [
+  const [rows, setRows] = useState([])
+  /*const rows = [
     createData('PDV', '7', 0, 0, 0, 0, 'R$ 1.680,00'),
     createData('PDV2', '5', 0, 0, 0, 0, 'R$ 1.350,00'),
-  ];
+  ];*/
 
   return (
     <TableContainer>
@@ -175,28 +221,28 @@ const TablePDV = () => {
           orderBy={orderBy}
           onRequestSort={handleRequestSort}
         />
-        <tbody>
-          {stableSort(tabelaData, getComparator(order, orderBy))
+        <tbody> 
+          {stableSort(pdvs, getComparator(order, orderBy))
             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
             .map((item, index) => (
-              <React.Fragment key={item.id}>
+              <React.Fragment key={item.pdvs}>
                 <tr
-                  className={index % 2 === 0 ? 'pdv-linha-branca' : 'pdv-linha-cinza'}
+                  className={index % 2 === 0 ? 'pdvs-linha-branca' : 'pdvs-linha-cinza'}
                 >
                   <td className="pdv-celula">
                     <button
                       className="pdv-botao-expandir"
-                      onClick={() => expandirLinha(item.id)}
+                      onClick={() => expandirLinha(item.pdvs)}
                     >
                       {item.expandir ? '-' : '+'}
                     </button>
                   </td>
                   <td className="pdv-celula">{item.pdv}</td>
-                  <td className="pdv-celula">{item.qtdeHoje}</td>
-                  <td className="pdv-celula">{item.valorHoje}</td>
-                  <td className="pdv-celula">{item.qtdeTotal}</td>
-                  <td className="pdv-celula">{item.valorTotal}</td>
-                  <td className="pdv-celula">{item.cortesia}</td>
+                  <td className="pdv-celula">{item.quant_hoje}</td>
+                  <td className="pdv-celula">{item.valor_hoje}</td>
+                  <td className="pdv-celula">{item.quant_total}</td>
+                  <td className="pdv-celula">{item.valor_total}</td>
+                  <td className="pdv-celula">{item.cortesias}</td>
                   <td className="pdv-celula-pgto">
                     <span className="pdv-celula-span">DIN</span>{item.pgto}<br/>
                     <span className="pdv-celula-span">CCR</span>{item.pgto}<br/>
@@ -217,16 +263,15 @@ const TablePDV = () => {
                       <td className="pdv-linha-azul">PGTO</td>
                       <td className="pdv-linha-azul">Valor</td>
                     </tr>
-                    {rows.map((row) => (
-                      <tr key={row.pdv}>
+                    {pdvs.map((row) => (
+                      <tr key={row.pdvs}>
                         <td className="pdv-conteudo-expandido"></td>
-                        <td className="pdv-conteudo-expandido">{row.pdv}</td>
-                        <td className="pdv-conteudo-expandido">{row.vendas}</td>
-                        <td className="pdv-conteudo-expandido">{row.cortesias}</td>
-                        <td className="pdv-conteudo-expandido">{row.impressos}</td>
-                        <td className="pdv-conteudo-expandido">{row.cancelados}</td>
-                        <td className="pdv-conteudo-expandido">{row.pgto}</td>
-                        <td className="pdv-conteudo-expandido">{row.valor}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.classe}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.quant_hoje}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.valor_hoje}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.quant_total}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.valor_total}</td>
+                        <td className="pdv-conteudo-expandido">{row.classes.cortesias}</td>
                       </tr>
                     ))}
                   </>
@@ -240,7 +285,7 @@ const TablePDV = () => {
         labelDisplayedRows={({ from, to, count }) => `${from}-${to} de ${count}`}
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={tabelaData.length}
+        count={pdvs.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
